b3ceac12c82660b677ea615f0649d1993aff72e1
oleg <ghimpco@gmail.com>
2023-03-18 14:27:15 +0200

    main

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from HeapSort import HeapSort
 
 if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7, ]
-    app = HeapSort()
-    app.heap_sort(arr)
-    n = len(arr)
-    print('Sorted array is')
-    for i in range(n):
-        print(arr[i], end=' ')
+    print(1)
